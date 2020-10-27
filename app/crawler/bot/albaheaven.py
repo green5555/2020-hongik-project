4571e6737c8c24b59fa4e54a6b73fb7f793c1502
Green Jung <pasdfq@naver.com>
2020-10-27 19:41:13 +0900

    update heaven crawler

--- app/crawler/bot/albaheaven.py
+++ app/crawler/bot/albaheaven.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup, NavigableString
 import time
 from datetime import datetime
+from itertools import chain 
 import pickle
 
 class AlbaheavenCrawler :
@@ -12,31 +13,43 @@ class AlbaheavenCrawler :
     def crawl_from_site(self) :
         PAGE_NUM = 1
         SLEEP_TIME = 5.0
-
+        
         detail_url_list = []
         for page in range(1, 1+PAGE_NUM) :
             time.sleep(SLEEP_TIME)
-            source = requests.get(self.base_url + f'/job/Main.asp?page={page}').text
-            soup = BeautifulSoup(source, 'html.parser')
-            detail_url_list.append(soup.select(
-                    '#NormalInfo > table > tbody > tr.firstLine > td.title > span > a.applBtn.blankView')[0]['href'])
-            for i in range(3, 10, 2):
-            #for i in range(3, 101, 2) :
+            try :
+                source = requests.get(self.base_url + f'/job/Main.asp?page={page}').text
+                soup = BeautifulSoup(source, 'html.parser')
                 detail_url_list.append(soup.select(
-                    f'#NormalInfo > table > tbody > tr:nth-child({i}) > td.title > span > a.applBtn.blankView')[0]['href'])
+                        '#NormalInfo > table > tbody > tr.firstLine > td.title > span > a.applBtn.blankView')[0]['href'])
+                print(f'successfully crawl page {page}')
+            except Exception as e:
+                print(f'Fail to crawl page {page} : {e}')
+
+            for i in range(3, 7, 2):
+            # for i in range(3, 101, 2):
+                try :
+                    detail_url_list.append(soup.select(
+                        f'#NormalInfo > table > tbody > tr:nth-child({i}) > td.title > span > a.applBtn.blankView')[0]['href'])
+                except Exception as e:
+                    print(f'Fail to crawl page {page} - {i}th content : {e}')
 
         for i in detail_url_list :
-            self.detail_source_list.append((i, requests.get(self.base_url + i).text))
+            try :
+                self.detail_source_list.append((i, requests.get(self.base_url + i).text))
+            except Exception as e:
+                print(f'Fail to request detail url {i} - {e}')
             time.sleep(SLEEP_TIME)
+            
 
     def norm_sex(self, sex) :
-        if sex == '성별무관' :
+        if sex.count('무관') :
             return '무관'
         return sex
 
     def norm_age(self, age) :
-        if age == "연령무관" :
-            return (None, None)
+        if age.count('무관') :
+            return (0, 200) 
         s_age = age.split()
         l = s_age[0]
         l = int(l[:l.find('세')])
@@ -47,61 +60,57 @@ class AlbaheavenCrawler :
             r = int(r[:r.find('세')])
         return (l, r)
 
-    def norm_pay(self, pay) :
-        return pay[:-1].replace(',', '')
-
     def norm_worktime(self, worktime) :
         if worktime.startswith('시간협의') : 
             return (None, None)
         else :
-            (l,r) = (worktime[:5], worktime[7:13])
+            (l,r) = (worktime[:5], worktime[worktime.find('~')+1:worktime.find('~')+5])
             l = int(l[:l.find(':')])*60 + int(l[l.find(':')+1:])
             r = int(r[:r.find(':')])*60 + int(r[r.find(':')+1:])
             return (l,r)
+        
+    def parse_pay_tag(self, tag) :
+        return (tag.i.text, tag.strong.text.replace(',', ''))
 
     def get_text_except_child_from_tag(self, tag) :
         return [element for element in tag if isinstance(element, NavigableString)][0].strip()
 
     def get_info_from_source(self, alba_site_number, source) :
-        '''
-        source : 주소 url에 request를 날린 text
-        return : dict
-                {
-                'sex' : str, '남자', '여자', '무관'
-                'age' : tuple(int, int), (낮은 나이, 높은 나이), 없으면 None
-                'address' : str
-                'pay' : int
-                'type_of_pay' : str, '월급', '일급', etc
-                'worktime' : tuple(int, int), (시작 시간의 분, 끝 시간의 분)
-                'alba_site_name' : str
-                'alba_site_number' : str
-                }
-        '''
+        cond_selector = [
+            '#DetailView > div.detail-content > div:nth-child(2) > div > div.detail-content__condition--first > div.detail-content__condition-list',
+            '#DetailView > div.detail-content > div:nth-child(2) > div > div.detail-content__condition--first > div.detail-content__condition-list.detail-content__list--last',
+            '#InfoWork > div'
+        ]
+        
         soup = BeautifulSoup(source, 'html.parser')
         
-        sex_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSub.infoJob > div.infoQualify > ul > li.sex'
-        age_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSub.infoJob > div.infoQualify > ul > li.agetemp'
-        week_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSide.infoJob > div > ul > li.workweekcd > a'
-        address_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSide.infoJob > div > ul > li.address.divide > strong'
-        pay_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSide.infoJob > div > ul > li.getPay.divide > p.pay > strong'
-        type_of_pay_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSide.infoJob > div > ul > li.getPay.divide > p.pay > img'
-        worktime_selector = '#DetailView > div.wrapContents > div.infoSubSide > div.infoSide.infoJob > div > ul > li.worktimecd'
-        
         info = {}
-        info['sex'] = self.norm_sex(self.get_text_except_child_from_tag(soup.select(sex_selector)[0]))
-        info['age'] = self.norm_age(self.get_text_except_child_from_tag(soup.select(age_selector)[0]))
-        info['address'] = self.get_text_except_child_from_tag(soup.select(address_selector)[0])
-        info['pay'] = self.norm_pay(soup.select(pay_selector)[0].text)
-        info['type_of_pay'] = soup.select(type_of_pay_selector)[0]['alt']
-        info['worktime'] = self.norm_worktime(self.get_text_except_child_from_tag(soup.select(worktime_selector)[0]))
-        info['alba_site_name'] = 'albaheaven'
-        info['alba_site_number'] = alba_site_number[alba_site_number.find('adid=')+5:]
-        print(info)
+        for selector in cond_selector:
+            tags = soup.select(selector)[0].find_all('dl')
+            for tag in tags :
+                (dt, dd) = tag.dt.text, tag.dd.text
+                if dt == '성별' :
+                    info['sex'] = self.norm_sex(dd)
+                if dt == '연령' :
+                    info['age'] = self.norm_age(dd)
+                if dt == '근무지주소' :
+                    info['address'] = dd
+                if dt == '급여' :
+                    (info['type_of_pay'], info['pay']) = self.parse_pay_tag(tag.dd)
+                if dt == '근무시간' :
+                    info['worktime'] = self.norm_worktime(dd)
+        info['alba_site_name'] = '알바천국'
+        info['alba_site_number'] = alba_site_number[alba_site_number.find('adid=')+5:alba_site_number.find('&list')]
         return info
+
 
     def get_info_list(self) :
         self.crawl_from_site()
         result = []
         for i in self.detail_source_list :
-            result.append(self.get_info_from_source(i[0], i[1]))
+            try :
+                result.append(self.get_info_from_source(i[0], i[1]))
+            except Exception as e:
+                print(f'Fail to parse {i[1]} : {e}')
+                pass
         return result
--- app/crawler/views.py
+++ app/crawler/views.py
@@ -7,17 +7,21 @@ from .bot.albaheaven import AlbaheavenCrawler
 
 def insert_db(info_result):
     for info in info_result :
-        AlbaInfo.objects.create(
-            sex = info['sex'],
-            age_lower_bound = info['age'][0],
-            age_upper_bound = info['age'][1],
-            address = info['address'],
-            pay = info['pay'],
-            worktime_start = info['worktime'][0],
-            worktime_end = info['worktime'][1],
-            alba_site_name = info['alba_site_name'],
-            alba_site_number = info['alba_site_number']
-        )
+        try :
+            AlbaInfo.objects.create(
+                sex = info['sex'],
+                age_lower_bound = info['age'][0],
+                age_upper_bound = info['age'][1],
+                address = info['address'],
+                type_of_pay = info['type_of_pay'],
+                pay = info['pay'],
+                worktime_start = info['worktime'][0],
+                worktime_end = info['worktime'][1],
+                alba_site_name = info['alba_site_name'],
+                alba_site_number = info['alba_site_number']
+            )
+        except :
+            pass
 
 def make_db(request) :
     for a in AlbaInfo.objects.all():
@@ -27,11 +31,13 @@ def make_db(request) :
     albaheaven_info_result = albaheaven_crawler.get_info_list()
     insert_db(albaheaven_info_result)
 
-    albamon_crawler = AlbamonCrawler()
-    albamon_info_result = albamon_crawler.get_info_list()
-    insert_db(albamon_info_result)
+    # albamon_crawler = AlbamonCrawler()
+    # albamon_info_result = albamon_crawler.get_info_list()
+    # insert_db(albamon_info_result)
 
-    return HttpResponse(str(albamon_info_result) + str(albaheaven_info_result))
+    ret = 'Done!\n'
+    ret += f'heaven : {len(albaheaven_info_result)}\n'
+    return HttpResponse(str(albaheaven_info_result))
 
 def crawling(request):
     return make_db(request)
